9bdfd8b5a7d606c7bb084f02670bc94ab7ed0e25
soyluisarrieta <luisarrieta796@gmail.com>
2025-04-06 18:37:16 -0500

    feat: add Tailwind CSS, dark mode, and UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function App () {
   return (
-    <div>App</div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
