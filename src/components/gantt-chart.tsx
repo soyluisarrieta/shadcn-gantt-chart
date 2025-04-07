d4168a7147ee5f875560350b88cecf5cc2c3ec01
soyluisarrieta <luisarrieta796@gmail.com>
2025-04-06 23:39:48 -0500

    feat: add GanttSidebar component and useGanttContext hook

--- src/components/gantt-chart.tsx
+++ src/components/gantt-chart.tsx
@@ -16,27 +16,59 @@ const GanttContext = React.createContext<{
   startDate: Date;
 } | null>(null)
 
+const useGanttContext = () => {
+  const context = React.useContext(GanttContext)
+  if (!context) {
+    throw new Error('useGanttContext must be used within a GanttProvider')
+  }
+  return context
+}
+
+function GanttSidebar ({ width = '16vw' }) {
+  const { data } = useGanttContext()
+
+  return (
+    <div className="border-r flex-none" style={{ width }}>
+      <div className="h-8 flex items-center font-medium m-2">Lista de Tareas</div>
+
+      <div className="mt-8 border-t p-2">
+        {data.map(({ icon: Icon, ...item }) => (
+          <div key={item.id} className="h-8 flex items-center text-sm mb-2">
+            {Icon && (
+              <span className="mr-2">
+                {typeof Icon === 'function' ? <Icon /> : Icon}
+              </span>
+            )}
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
 function GanttChart ({
   className,
   children,
   data
-}:{
+}: {
   className?: string;
   children: React.ReactNode;
   data: GanttData[];
 }) {
-  const startDate = data.reduce((earliest, item) =>
-    item.startDate < earliest ? item.startDate : earliest, data[0].startDate)
+  const startDate = data.reduce(
+    (earliest, item) => (item.startDate < earliest ? item.startDate : earliest),
+    data[0].startDate
+  )
 
   return (
     <GanttContext.Provider value={{ data, startDate }}>
       <div className={cn('flex flex-1', className)}>
+        <GanttSidebar />
         {children}
       </div>
     </GanttContext.Provider>
   )
 }
 
-export {
-  GanttChart
-}
+export { GanttChart }
